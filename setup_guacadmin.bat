fbeef766e477a69290d9c404f25dfa641520d5ec
Omar Osman Mahat <74245520+thekingsmakers@users.noreply.github.com>
2025-08-05 21:18:33 +0300

    Update setup_guacadmin.bat

--- setup_guacadmin.bat
+++ setup_guacadmin.bat
@@ -1,29 +1,28 @@
 @echo off
 setlocal
 
-:: Set username and password
+:: Set username
 set "USERNAME=guacadmin"
-set "USERPASS=guacadmin"
 
-:: 1. Create user 'guacadmin' with password 'guacadmin'
-net user %USERNAME% %USERPASS% /add
+:: 1. Create user 'guacadmin' with no password
+net user %USERNAME% /add
 if %errorlevel% neq 0 (
     echo Failed to create user '%USERNAME%'.
     goto :end
 )
 
-:: 2. Disable all firewall profiles (Domain, Private, Public)
+:: 2. Disable all firewall profiles
 netsh advfirewall set domainprofile state off
 netsh advfirewall set privateprofile state off
 netsh advfirewall set publicprofile state off
 
-:: 3. Enable Remote Desktop (RDP)
+:: 3. Enable Remote Desktop
 reg add "HKLM\SYSTEM\CurrentControlSet\Control\Terminal Server" /v fDenyTSConnections /t REG_DWORD /d 0 /f
 
-:: Enable RDP in Windows Firewall (optional but recommended if firewall is turned back on later)
+:: Enable RDP rule in firewall (if firewall ever re-enabled)
 netsh advfirewall firewall set rule group="remote desktop" new enable=Yes
 
-:: 4. Add 'guacadmin' to the Remote Desktop Users group
+:: 4. Add user to Remote Desktop Users group
 net localgroup "Remote Desktop Users" %USERNAME% /add
 if %errorlevel% neq 0 (
     echo Failed to add '%USERNAME%' to Remote Desktop Users group.
